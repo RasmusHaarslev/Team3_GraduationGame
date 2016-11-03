67cda06d579244c75f8a6848271ae2375103ef7b
Nils Rungholm Jensen <nilsrungholmjensen@gmail.com>
2016-11-03 14:02:42 +0100

    Added a test-environment, added early placeholder models for players, and modified the camera-script to test some things out.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class FollowPlayer : MonoBehaviour {
 
 	GameObject player;
+	private Vector3 positionPrev; //stored position of the player, from last step.
     public Vector3 CameraPositionInRelationToPlayer = new Vector3(0, 12, -13);
+	public float CameraMoveOffset = 1.0f;
+	public float CameraMoveSnappyness = 0.015f;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
+		positionPrev = player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = player.transform.position + CameraPositionInRelationToPlayer;
+		Vector3 playerRelativeMovement = (player.transform.position - positionPrev) / Time.deltaTime;
+		Vector3 targetCamPosition = player.transform.position + CameraPositionInRelationToPlayer + new Vector3(playerRelativeMovement.x,0,playerRelativeMovement.z) *CameraMoveOffset;
+
+		positionPrev = player.transform.position;
+
+		transform.position += (targetCamPosition - transform.position) * 0.015f;
 	}
 }
