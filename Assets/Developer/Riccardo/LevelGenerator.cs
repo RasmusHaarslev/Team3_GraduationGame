40d1d94ee8253fa6a4ca2e27c10769038276b972
Riccardo Loggini <magiclogins@gmail.com>
2016-11-07 15:17:58 +0100

    First iteration of Database implementation.

---
+++ Assets/Developer/Riccardo/LevelGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+	
+        //TODO acquire data from playerprefs
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="difficultyLevel">The input variable to determine the strength of characters in the scene.</param>
+    /// <param name="wolfPackCount">How many Points of interests are filled with wolves.</param>
+    /// <param name="tribesmanPackCount">How many Points of interests are filled with tribesmen.</param>
+    /// <param name="lootCount">Quantity of loot acquired if the scene is completed.</param>
+    /// <param name="environmentType">The index for the visual appearance of the environment objects on the scene</param>
+    public void Init(int difficultyLevel, int wolfPackCount, int tribesmanPackCount, int lootCount, int environmentType)
+    {
+        
+    }
+
+
+    public void getCharacterByName(string characterName)
+    {
+        //get informations from 
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class Character : MonoBehaviour
+{
+    //values gained from the database
+    public CharacterValues characterBaseValues;
+    //combat values
+    public int health = 0;
+    public int damage = 0;
+    public int range = 0;
+    public int damageSpeed = 0;
+
+    
+    //Combat state values
+    private bool isInCombat = false;
+    //model values
+    private Dictionary<string, Transform> slots;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        slots = new Dictionary<string, Transform>(){ //TODO: chage gameObject of this list
+        {"head", transform },
+        {"torso", transform },
+        {"leftHand", transform },
+        {"rightHand", transform },
+        {"legs", transform }
+    };
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Set the character values passed in the parameter
+    /// </summary>
+    /// <param name="initValues"></param>
+    void init(CharacterValues initValues)
+    {
+        Type typeB = initValues.GetType();
+        foreach (PropertyInfo property in GetType().GetProperties())
+        {
+            if (!property.CanRead || (property.GetIndexParameters().Length > 0))
+                continue;
+
+            PropertyInfo other = typeB.GetProperty(property.Name);
+            if ((other != null) && (other.CanWrite))
+                other.SetValue(initValues, property.GetValue(this, null), null);
+        }
+
+    }
+
+    /// <summary>
+    /// Changes the stats and relocate the item on the right character slot
+    /// </summary>
+    /// <param name="item"></param>
+    /// <param name="slot"></param>
+    void equipItem(EquippableItem item, int slot = 0)
+    {
+        //change character parameters
+        health += item.itemValues.health;
+        damage += item.itemValues.damage;
+        damageSpeed += item.itemValues.damageSpeed;
+        if (item.itemValues.type == "hand" && slot == 1) range = item.itemValues.range; //change range only if it is the right hand
+        //parent the item to the character
+        item.transform.parent = slots[item.itemValues.slot];
+        item.transform.localPosition = Vector3.zero;
+
+
+    }
+
+
+
+}
