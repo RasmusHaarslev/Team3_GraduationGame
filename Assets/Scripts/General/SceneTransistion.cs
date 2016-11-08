cc453cef58834d46bdfa6bb0c50eb18bd32490c1
Christian Hansen <chr.hansen@live.dk>
2016-11-08 14:35:29 +0100

    merge

---
+++ Assets/Scripts/General/SceneTransistion.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneTransistion : MonoBehaviour
+{
+
+    public static SceneTransistion instance;
+
+    public float transitionTime = 1.0f;
+
+    public bool fadeIn;
+    public bool fadeOut;
+
+    public Image fadeImg;
+
+    float time = 0f;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            instance = this;
+            if (fadeIn)
+            {
+                fadeImg.color = new Color(fadeImg.color.r, fadeImg.color.g, fadeImg.color.b, 1.0f);
+            }
+        }
+        else
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        if (fadeIn)
+        {
+            StartCoroutine(StartScene());
+        }
+    }
+
+    //This gets called from anywhere you need to load a new scene
+    public void LoadScene(int level)
+    {
+        StartCoroutine(EndScene(level));
+    }
+
+    IEnumerator StartScene()
+    {
+        time = 1.0f;
+        yield return null;
+        while (time >= 0.0f)
+        {
+            fadeImg.color = new Color(fadeImg.color.r, fadeImg.color.g, fadeImg.color.b, time);
+            time -= Time.deltaTime * (1.0f / transitionTime);
+            yield return null;
+        }
+        fadeImg.gameObject.SetActive(false);
+    }
+    
+    IEnumerator EndScene(int nextScene)
+    {
+        fadeImg.gameObject.SetActive(true);
+        time = 0.0f;
+        yield return null;
+        while (time <= 1.0f)
+        {
+            fadeImg.color = new Color(fadeImg.color.r, fadeImg.color.g, fadeImg.color.b, time);
+            time += Time.deltaTime * (1.0f / transitionTime);
+            yield return null;
+        }
+        SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+        StartCoroutine(StartScene());
+    }
+}
--- Assets/Scripts/General/StringResources.cs
+++ Assets/Scripts/General/StringResources.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/// <summary>
+/// This class gather all the main resources paths, to be used in other scripts.
+/// </summary>
 public class StringResources
 {
 
     public static string characterPrefabsPath = "Prefabs/Prototype/";
-	// Use this for initialization
-	void Start () {
+
+    public static string equippableItemsPrefabsPath = "Prefabs/Items/EquippableItems";
+
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
