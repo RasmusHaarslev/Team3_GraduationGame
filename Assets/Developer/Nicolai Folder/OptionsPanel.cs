b9b3ad9f5e6a15fbf2a23b9a6c41df8ea8ca4257
John <irelandjj@gmail.com>
2016-11-10 15:15:27 +0100

    CAMP

---
+++ Assets/Developer/Nicolai Folder/OptionsPanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsPanel : MonoBehaviour {
+
+
+    private bool panelOpen = false;
+    public GameObject OptionPanel;
+
+    public void TogglePanelOpen()
+    {
+        panelOpen = !panelOpen;
+        SetPanelVisible();
+    }
+
+    public void SetPanelVisible()
+    {
+        OptionPanel.SetActive(panelOpen);
+    }
+
+}
