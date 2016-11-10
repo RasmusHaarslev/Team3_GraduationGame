69004c2ba27d59670a4f31f667f68b3f58e05ac3
Peter Scheel <selech@live.dk>
2016-11-10 15:30:51 +0100

    Work on scene initializor

--- Assets/Scripts/Controllers/SceneInitializor.cs
+++ Assets/Scripts/Controllers/SceneInitializor.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class SceneInitializor : MonoBehaviour {
 
-    [Header("UI prefabs")]
-    public GameObject Camera;
+    [Header("Camera prefabs")]
+    public GameObject CameraPrefab;
 
     [Header("UI prefabs")]
     public GameObject[] UIPrefabs;
 
     // Use this for initialization
     void Start() {
-        
+        GameObject _camera = (GameObject)Instantiate(CameraPrefab);
+
+        //_camera.GetComponent<CameraController>().
 
         foreach (GameObject ui in UIPrefabs) { 
             GameObject _ui = (GameObject)Instantiate(ui);
