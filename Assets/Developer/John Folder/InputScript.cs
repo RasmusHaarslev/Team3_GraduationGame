7c9a565ac72cc55026a833a9b6f599c647ed0bbf
Veso <veselin.yovchev@hotmail.com>
2016-11-10 16:46:12 +0100

    revert

--- Assets/Developer/John Folder/InputScript.cs
+++ Assets/Developer/John Folder/InputScript.cs
@@ -5,56 +5,73 @@ using UnityEngine.EventSystems;
 
 public class InputScript : MonoBehaviour
 {
-    public float timeMax = 0.25f; //Time for command to trigger
-    float timer = 0f; // 
-    float countdown;
-    public bool buttonClicked = false; //if button is clicked
+	public float timeMax = 0.25f; //Time for command to trigger
+	float timer = 0f; // 
+	float countdown;
+	public bool buttonClicked = false; //if button is clicked
 
-    public GameObject commandPanel;
+	public GameObject commandPanel;
 
-    CommandsManager commandsManager;
+	CommandsManager commandsManager;
+	MoveScript moveScript;
 
-    public void ButtonDown()
-    {
-        Debug.Log("clicked");
-        buttonClicked = true;
-        countdown = timeMax;
-        
-    }
+	void Start()
+	{
 
-    public void ButtonUp()
-    {
+	}
 
-        buttonClicked = false;
-        if (commandPanel.activeSelf == true)
-        {
-            commandsManager = transform.parent.GetComponentInChildren<CommandsManager>();
-            commandsManager.currentCommand.Clear();
-            commandsManager.previousIndex = -1;
+	public void ButtonDown()
+	{
+		Debug.Log("clicked");
+		buttonClicked = true;
+		countdown = timeMax;
+	}
 
-            commandPanel.GetComponent<LineRenderer>().SetVertexCount(0);
-            commandPanel.GetComponent<DrawLine>().centerAdded = false;
-            commandPanel.GetComponent<DrawLine>().drawArray = new System.Collections.Generic.List<Vector3>();
+	public void ButtonUp()
+	{
 
-            commandPanel.SetActive(false);
-            
-        }
+		buttonClicked = false;
+		if (commandPanel.activeSelf == true)
+		{
+			commandsManager = transform.parent.GetComponentInChildren<CommandsManager>();
+			commandsManager.currentCommand.Clear();
+			commandsManager.previousIndex = -1;
 
-    }
+			commandPanel.GetComponent<LineRenderer>().SetVertexCount(0);
+			commandPanel.GetComponent<DrawLine>().centerAdded = false;
+			commandPanel.GetComponent<DrawLine>().drawArray = new System.Collections.Generic.List<Vector3>();
 
-    void Update()
-    {
-        if (buttonClicked && commandPanel.activeSelf == false)
-        {
-            countdown = countdown - Time.deltaTime;
+			commandPanel.SetActive(false);
+		}
 
-            if (countdown < 0)
-            {
-                commandPanel.SetActive(true);
-                commandPanel.GetComponent<DrawLine>().countVertices++;
-                commandPanel.GetComponent<LineRenderer>().SetVertexCount(1);
-                   commandPanel.GetComponent<LineRenderer>().SetPosition(0, new Vector3(0, 0, commandPanel.GetComponent<DrawLine>().zDistance));
-            }
-        }
-    }
+	}
+
+	void Update()
+	{
+
+		if (moveScript == null)
+		{
+			moveScript = GameObject.FindGameObjectWithTag("Player").GetComponent<MoveScript>();
+		}
+		if (commandPanel.activeSelf == true)
+		{
+			moveScript.movement = false;
+		} else
+		{
+			moveScript.movement = true;
+		}
+		if (buttonClicked && commandPanel.activeSelf == false)
+		{
+
+			countdown = countdown - Time.deltaTime;
+
+			if (countdown < 0)
+			{
+				commandPanel.SetActive(true);
+				commandPanel.GetComponent<DrawLine>().countVertices++;
+				commandPanel.GetComponent<LineRenderer>().SetVertexCount(1);
+				commandPanel.GetComponent<LineRenderer>().SetPosition(0, new Vector3(0, 0, commandPanel.GetComponent<DrawLine>().zDistance));
+			}
+		}
+	}
 }
---
+++ Assets/ModuleDesigner/Scripts/GroupModule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using Assets.ModuleDesigner.Scripts.BaseClasses;
+using UnityEngine;
+
+namespace Assets.ModuleDesigner.Scripts
+{
+    public class GroupModule : TriggerReceiver
+    {
+        [Header("Output objects")]
+        public TriggerReceiver[] Targets;
+
+        public override void TriggerEnter()
+        {
+            foreach (var target in Targets)
+            {
+                target.TriggerEnter();
+            }
+        }
+
+        public override void TriggerExit()
+        {
+            foreach (var target in Targets)
+            {
+                target.TriggerExit();
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            if (KeepGizmo)
+                OnDrawGizmosSelected();
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawMesh(gizmoMesh, transform.position, transform.rotation, Vector3.one);
+
+            foreach (var obj in Targets) 
+            {
+                obj.ShowGizmos();
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(this.transform.position + new Vector3(0, 0.5f, 0), obj.transform.position - new Vector3(0, 0.5f, 0));
+            }
+        }
+
+        public override void Expose(GameObject go)
+        {
+
+        }
+
+        public override void ShowGizmos()
+        {
+            OnDrawGizmos();
+        }
+    }
+}
