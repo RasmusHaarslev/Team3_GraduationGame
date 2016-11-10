7c7fd8011d691469e8ffcef9eec67269afacc87c
John <irelandjj@gmail.com>
2016-11-10 17:30:38 +0100

    camp mouse sstuff

--- Assets/Developer/Nicolai Folder/MouseManager.cs
+++ Assets/Developer/Nicolai Folder/MouseManager.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class MouseManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public GameObject selectedObject;
+    public GameObject hoveredObject;
+
+    // Update is called once per frame
+    void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray, out hitInfo))
+        {
+            GameObject hitObject = hitInfo.transform.gameObject;
+            Debug.Log("mouse is over: " + hitObject.name);
+            Debug.Log("collider is: " + hitInfo.collider.name);
+            SelectObject(hitObject);
+        }
+        else
+        {
+            ClearSelection();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectedObject = hoveredObject;
+            Debug.Log("selected object is: "+ selectedObject);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void SelectObject(GameObject obj)
+    {
+        if(selectedObject != null)
+        {
+            if(obj == selectedObject)
+            {
+                return;
+            }
+
+            ClearSelection();
+        }
+
+        hoveredObject = obj;
+    }
+
+    void ClearSelection()
+    {
+        selectedObject = null;
+    }
 }
