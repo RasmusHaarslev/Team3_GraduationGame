1c0ee31e8aa1756a907fbd387832aacb79545b81
John <irelandjj@gmail.com>
2016-11-14 11:56:06 +0100

    DB in campscene

---
+++ Assets/Developer/Nicolai Folder/PanelStuff/PanelScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PanelScript : MonoBehaviour {
+
+    public List<GameObject> panelList = new List<GameObject>();
+
+}
