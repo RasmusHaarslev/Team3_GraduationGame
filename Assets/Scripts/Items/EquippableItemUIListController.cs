343102e1c9dacab295f2202586c0f53510d2b19b
Riccardo Loggini <magiclogins@gmail.com>
2016-11-16 11:09:54 +0100

    Initial classes for a scrollable list of items

---
+++ Assets/Scripts/Items/EquippableItemUIListController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EquippableItemListController : MonoBehaviour
+{
+
+    public Sprite[] AnimalImages;
+    public GameObject ContentPanel;
+    public GameObject ListItemPrefab;
+
+    ArrayList itemsValues;
+
+    void Start()
+    {
+        ListItemPrefab = Resources.Load(StringResources.uiPrefabsPath + "EquippableItemUIScrollElement") as GameObject;
+        // 1. Get the data to be displayed
+        itemsValues = 
+
+        // 2. Iterate through the data, 
+        //	  instantiate prefab, 
+        //	  set the data, 
+        //	  add it to panel
+        foreach (EquippableitemValues values in itemsValues)
+        {
+            GameObject newAnimal = Instantiate( ListItemPrefab) as GameObject;
+            ListItemController controller = newAnimal.GetComponent();
+            controller.Icon.sprite = animal.Icon;
+            controller.Name.text = animal.Name;
+            controller.Description.text = animal.Description;
+            newAnimal.transform.parent = ContentPanel.transform;
+            newAnimal.transform.localScale = Vector3.one;
+        }
+    }
+}
---
+++ Assets/Scripts/Items/UIListEquippableItemController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ListItemController : MonoBehaviour
+{
+
+    public Image Icon;
+    public Text Name, Description;
+
+}
