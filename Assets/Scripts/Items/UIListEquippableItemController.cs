cbb7bfafae8aad3abca7727a649a46aabb58aa45
Riccardo Loggini <magiclogins@gmail.com>
2016-11-16 11:33:03 +0100

    Progress with displaying equippable Items List

--- Assets/Scripts/Items/UIListEquippableItemController.cs
+++ Assets/Scripts/Items/UIListEquippableItemController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class ListItemController : MonoBehaviour
+public class UIListEquippableItemController : MonoBehaviour
 {
 
     public Image Icon;
