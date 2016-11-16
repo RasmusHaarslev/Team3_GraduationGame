c163ea72cc82b2005932949da13721f1e4b018cc
Christian Hansen <chr.hansen@live.dk>
2016-11-16 14:00:58 +0100

    Remove unimplemented traits

--- Assets/Scripts/Characters/CharacterValues.cs
+++ Assets/Scripts/Characters/CharacterValues.cs
@@ -7,25 +7,21 @@ using SQLite;
 /// </summary>
 public class CharacterValues
 {
-    [PrimaryKey, AutoIncrement]
-    public int id { get; set; }
-    public string name { get; set; }
-    public type Type { get; set; }
-    public string description { get; set; }
-    public int damage { get; set; }
-    public int health { get; set; }
-    public int damageSpeed { get; set; }
-    public int range { get; set; }
+	[PrimaryKey, AutoIncrement]
+	public int id { get; set; }
+	public string name { get; set; }
+	public type Type { get; set; }
+	public string description { get; set; }
+	public int damage { get; set; }
+	public int health { get; set; }
+	public int damageSpeed { get; set; }
+	public int range { get; set; }
 	public int tier { get; set; }
 
-    public TargetTrait targetTrait { get; set; }
-    public CombatTrait combatTrait { get; set; }
-    /*
-    public combatFocusType CombatFocusType { get; set; }
-    public combatFleeType CombatFleeType { get; set; }
-    public outOfCombatMovementType OutOfCombatMovementType { get; set; }
-    */
-    public enum type
+	public TargetTrait targetTrait { get; set; }
+	public CombatTrait combatTrait { get; set; }
+
+	public enum type
 	{
 		Tribesman,
 		Wolf,
@@ -33,62 +29,31 @@ public class CharacterValues
 		Player
 	}
 
-    public enum TargetTrait
-    {
-        NoTrait,
-        Foolhardy,
-        StubbornDefender,
-        PacifistSoul,
-        Bully,
-        Codependant,
-        Helpful,
-        GlorySeeker,
-        LowAttentionSpan,
-        Loyal,
+	public enum TargetTrait
+	{
+		NoTrait,
+		Foolhardy,
+		StubbornDefender,
+		Bully,
+		Codependant,
+		GlorySeeker,
+		LowAttentionSpan,
+		Loyal
+	}
+	public enum CombatTrait
+	{
+		NoTrait,
+		BraveFool,
+		Fearful,
+		Clingy,
+		Desperate,
+		Vengeful,
+		VeryUnlikable
+	}
 
-    }
-    public enum CombatTrait
-    {
-        NoTrait,
-        BraveFool,
-        Fearful,
-        Excitable,
-        Clingy,
-        Desperate,
-        Vengeful,
-        VeryUnlikable
-    }
-
-    /*
-	public enum combatFocusType 
-    {
-        PlayerAttackers,
-        PlayerFocus,
-        BossTier,
-        HighTier,
-        MidTier,
-        LowTier,
-        Nearest,
-        Defensive
-    };
-
-public enum combatFleeType
-    {
-        Default,
-        Brave,
-        Fearful
-    };
-
-    public enum outOfCombatMovementType
-    {
-        StandStill,
-        FollowLeader,
-        Patrolling
-    }
-    */
-    public string prefabName { get; set; }
-    public override string ToString()
-    {
-        return string.Format("[CharacterInfo: Id={0}, Name={1},  prefabName={2}]", id, name, prefabName);
-    }
+	public string prefabName { get; set; }
+	public override string ToString()
+	{
+		return string.Format("[CharacterInfo: Id={0}, Name={1},  prefabName={2}]", id, name, prefabName);
+	}
 }
