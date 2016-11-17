16b7b193aaef8f4c0228da94ac58f2e64c554763
John <irelandjj@gmail.com>
2016-11-17 16:24:58 +0100

    fixing stufff

--- Assets/Developer/Nicolai Folder/PanelStuff/PanelController.cs
+++ Assets/Developer/Nicolai Folder/PanelStuff/PanelController.cs
@@ -24,6 +24,7 @@ public class PanelController : MonoBehaviour, IPointerClickHandler
 
         if (gameObject.CompareTag("Friendly") || gameObject.CompareTag("Player") && panelScript.panelList[1].activeSelf == false)
         {
+            Debug.Log(panelScript);
             panelScript.UpdateSoldierStats(gameObject);
             panelScript.ActivateCamera(gameObject);
             panelScript.panelList[3].SetActive(true);
