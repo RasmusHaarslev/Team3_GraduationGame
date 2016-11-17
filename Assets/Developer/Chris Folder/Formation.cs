018b48bc838f3fe4b77b0c8bdd7bd7bb4e95362a
Christian Hansen <chr.hansen@live.dk>
2016-11-17 15:51:35 +0100

    Fix formation bug

--- Assets/Developer/Chris Folder/Formation.cs
+++ Assets/Developer/Chris Folder/Formation.cs
@@ -57,9 +57,9 @@ public class Formation : MonoBehaviour
 			for(int i = 0; i < 3; i++)
 			{
 				if(rears[i] == false)
-					Move(i, frontPositions[frontPositions.IndexOf(formationPositions[followers[0]])]);
+					Move(i, frontPositions[frontPositions.IndexOf(formationPositions[followers[i]])]);
 				else
-					Move(i, rearPositions[rearPositions.IndexOf(formationPositions[followers[0]])]);
+					Move(i, rearPositions[rearPositions.IndexOf(formationPositions[followers[i]])]);
 			}
 		}
 	}
