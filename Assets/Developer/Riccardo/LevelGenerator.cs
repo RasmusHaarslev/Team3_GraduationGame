b159c6dafe87ac373c95ea13416edb6435e93377
Veso <veselin.yovchev@hotmail.com>
2016-11-18 13:16:45 +0100

    base ambience

--- Assets/Developer/Riccardo/LevelGenerator.cs
+++ Assets/Developer/Riccardo/LevelGenerator.cs
@@ -24,7 +24,7 @@ public class LevelGenerator : MonoBehaviour
        
 	    dataService.GetPlayerFellowshipInPosition(gameObject.GetComponentInChildren<FellowshipSpawnPoint>().transform);
         
-        
+		Manager_Audio.PlaySound (Manager_Audio.baseAmbience,this.gameObject);
 
 		//TODO acquire data from playerprefs
         
