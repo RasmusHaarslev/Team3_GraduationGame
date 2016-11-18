e5a71167745cc831ec781555dd99a3614fb8db84
Nicolai <nico_goal@hotmail.com>
2016-11-18 14:23:24 +0100

    Changes

--- Assets/Scripts/Level Selection/LevelSelectionGenerator.cs
+++ Assets/Scripts/Level Selection/LevelSelectionGenerator.cs
@@ -127,16 +127,16 @@ public class LevelSelectionGenerator : MonoBehaviour
                     increaseX = 0;
                     break;
                 case 2:
-                    startX = -241.5f;
-                    increaseX = 483f;
+                    startX = -178.3f;
+                    increaseX = 356.6f;
                     break;
                 case 3:
-                    startX = -483f;
-                    increaseX = 483f;
+                    startX = -356.6f;
+                    increaseX = 356.6f;
                     break;
                 case 4:
-                    startX = -724f;
-                    increaseX = 482f;
+                    startX = -535f;
+                    increaseX = 356.6f;
                     break;
             }
 
@@ -157,7 +157,7 @@ public class LevelSelectionGenerator : MonoBehaviour
             {
                 GameObject imageRow = Instantiate(rowImagePrefab);
                 ResetTransform(imageRow, row);
-                imageRow.transform.localPosition = new Vector3(imageRow.transform.position.x, 100f, 0);
+                imageRow.transform.localPosition = new Vector3(0, 100f, 0);
                 imageRow.GetComponent<AddImageRow>().InsertImage(entry.Value.Count, nodesInRows[entry.Key - 1].Count);
             }
 
@@ -200,16 +200,16 @@ public class LevelSelectionGenerator : MonoBehaviour
                     increaseX = 0;
                     break;
                 case 2:
-                    startX = -241.5f;
-                    increaseX = 483f;
+                    startX = -178.3f;
+                    increaseX = 356.6f;
                     break;
                 case 3:
-                    startX = -483f;
-                    increaseX = 483f;
+                    startX = -356.6f;
+                    increaseX = 356.6f;
                     break;
                 case 4:
-                    startX = -724f;
-                    increaseX = 482f;
+                    startX = -535f;
+                    increaseX = 356.6f;
                     break;
             }
 
@@ -251,7 +251,7 @@ public class LevelSelectionGenerator : MonoBehaviour
             {
                 GameObject imageRow = Instantiate(rowImagePrefab);
                 ResetTransform(imageRow, row);
-                imageRow.transform.localPosition = new Vector3(imageRow.transform.position.x, 100f, 0);
+                imageRow.transform.localPosition = new Vector3(0, 100f, 0);
                 imageRow.GetComponent<AddImageRow>().InsertImage(numOfLastLevels, numOfParents);
             }
 
