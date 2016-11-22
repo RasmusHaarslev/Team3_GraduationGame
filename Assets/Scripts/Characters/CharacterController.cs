f76082973958200fc0f81db699a6daf183e6e305
Peter Scheel <selech@live.dk>
2016-11-22 15:42:57 +0100

    no message

---
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class CharacterController
+{
+    public static List<CharacterXML> CharactersLoaded = new List<CharacterXML>();
+
+    public static void SaveCharacters(List<CharacterXML> characters)
+    {
+        var charactersXML = new CharactersXML();
+
+        foreach (CharacterXML entry in characters)
+        {
+            CharactersLoaded.Add(entry);
+            charactersXML.Characters.Add(entry);
+        }
+
+        var path = Path.Combine(PersistentData.GetPath(), "characters.xml");
+
+        var serializer = new XmlSerializer(typeof(CharactersXML));
+        var stream = new FileStream(path, FileMode.Create);
+        serializer.Serialize(stream, charactersXML);
+        stream.Close();
+    }
+
+    public static List<CharacterXML> LoadCharacters()
+    {
+        var path = Path.Combine(PersistentData.GetPath(), "characters.xml");
+
+        if (!File.Exists(path))
+        {
+            Debug.LogError("No levels generated, reset game");
+            return new List<CharacterXML>();
+        }
+
+        var serializer = new XmlSerializer(typeof(CharactersXML));
+        var stream = new FileStream(path, FileMode.Open);
+        var container = serializer.Deserialize(stream) as CharactersXML;
+        stream.Close();
+
+        CharactersLoaded = new List<CharacterXML>();
+
+        foreach (CharacterXML character in container.Characters)
+        {
+            CharactersLoaded.Add(character);
+        }
+
+        return CharactersLoaded;
+    }
+}
+
+public class CharactersXML
+{
+    [XmlArray("Characters")]
+    [XmlArrayItem("Character")]
+    public List<CharacterXML> Characters = new List<CharacterXML>();
+}
+
+public class CharacterXML
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public bool isMale { get; set; }
+    public type Type { get; set; }
+    public string description { get; set; }
+    public int damage { get; set; }
+    public int health { get; set; }
+    public float damageSpeed { get; set; }
+    public int range { get; set; }
+    public int tier { get; set; }
+
+    public TargetTrait targetTrait { get; set; }
+    public CombatTrait combatTrait { get; set; }
+
+    public enum type
+    {
+        Tribesman,
+        Wolf,
+        Hunter,
+        Player
+    }
+
+    public enum TargetTrait
+    {
+        NoTrait,
+        Codependant,
+        LowAttentionSpan,
+        Loyal
+    }
+    public enum CombatTrait
+    {
+        NoTrait,
+        BraveFool,
+        Fearful,
+        Clingy
+    }
+
+    public string prefabName { get; set; }
+    public string materialName { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("[CharacterInfo: Id={0}, Name={1},  prefabName={2}]", id, name, prefabName);
+    }
+}
---
+++ Assets/Scripts/Items/EquippableItemController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class ItemController
+{
+    public static List<ItemXML> ItemsLoaded = new List<ItemXML>();
+
+    public static void SaveCharacters(List<ItemXML> characters)
+    {
+        var itemsXML = new ItemsXML();
+
+        foreach (ItemXML entry in characters)
+        {
+            ItemsLoaded.Add(entry);
+            itemsXML.Items.Add(entry);
+        }
+
+        var path = Path.Combine(PersistentData.GetPath(), "characters.xml");
+
+        var serializer = new XmlSerializer(typeof(CharactersXML));
+        var stream = new FileStream(path, FileMode.Create);
+        serializer.Serialize(stream, itemsXML);
+        stream.Close();
+    }
+
+    public static List<ItemXML> LoadCharacters()
+    {
+        var path = Path.Combine(PersistentData.GetPath(), "characters.xml");
+
+        if (!File.Exists(path))
+        {
+            Debug.LogError("No levels generated, reset game");
+            return new List<ItemXML>();
+        }
+
+        var serializer = new XmlSerializer(typeof(ItemsXML));
+        var stream = new FileStream(path, FileMode.Open);
+        var container = serializer.Deserialize(stream) as ItemsXML;
+        stream.Close();
+
+        ItemsLoaded = new List<ItemXML>();
+
+        foreach (ItemXML character in container.Items)
+        {
+            ItemsLoaded.Add(character);
+        }
+
+        return ItemsLoaded;
+    }
+}
+
+public class ItemsXML
+{
+    [XmlArray("Items")]
+    [XmlArrayItem("Item")]
+    public List<ItemXML> Items = new List<ItemXML>();
+}
+
+public class ItemXML
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public type Type { get; set; }
+    public slot Slot { get; set; }
+    public int characterId { get; set; }
+    public string rarity { get; set; }
+    public int level { get; set; }
+    public string description { get; set; }
+    public int damage { get; set; }
+    public int health { get; set; }
+    public float damageSpeed { get; set; }
+    public int range { get; set; }
+    public string prefabName { get; set; }
+    public string materialName { get; set; }
+
+    public enum slot
+    {
+        head,
+        torso,
+        leftHand,
+        rightHand,
+        legs
+    }
+    public enum type
+    {
+        polearm,
+        shield,
+        rifle
+    }
+    public override string ToString()
+    {
+        return string.Format("[EquippableitemValues: Id={0}, Name={1},  prefabName={2}]", id, name, prefabName);
+    }
+}
