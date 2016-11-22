246ad5eb74b440962f54c6441d58ac2068ebc0c3
Riccardo Loggini <magiclogins@gmail.com>
2016-11-22 12:06:02 +0100

    tutorial panel

---
+++ Assets/Scripts/Tutorial/CampTutorialController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CampTutorialController : MonoBehaviour
+{
+
+    public List<GameObject> panelList = new List<GameObject>();
+    private int currentPanel;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPanel = 0;
+    }
+
+    public void NextPanel()
+    {
+        panelList[currentPanel].SetActive(false);
+        panelList[++currentPanel].SetActive(true);
+    }
+}
