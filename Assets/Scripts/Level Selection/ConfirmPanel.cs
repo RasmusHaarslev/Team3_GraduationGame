124c289b839d3e68f5c837ba3583ba4a27bef3a2
Nicolai <nico_goal@hotmail.com>
2016-11-24 11:50:06 +0100

    Fixed popup

--- Assets/Scripts/Level Selection/ConfirmPanel.cs
+++ Assets/Scripts/Level Selection/ConfirmPanel.cs
@@ -10,8 +10,7 @@ public class ConfirmPanel : MonoBehaviour {
     public Text txtBtnYes;
     public Text txtBtnNo;
 
-    public Text txtPlay;
-    public Text txtScout;    
+    public Text txtHeader;  
 
     public void SetupText(GameObject node, string mode)
     {
@@ -19,10 +18,10 @@ public class ConfirmPanel : MonoBehaviour {
         txtBtnNo.text = TranslationManager.Instance.GetTranslation("No");
         if (mode == "play")
         {
-            txtPlay.text = TranslationManager.Instance.GetTranslation("ConfirmSure") + " " + node.GetComponent<Node>().TravelCost + " " + TranslationManager.Instance.GetTranslation("OnPlaying");
+            txtHeader.text = TranslationManager.Instance.GetTranslation("ConfirmSure") + " " + node.GetComponent<Node>().TravelCost + " " + TranslationManager.Instance.GetTranslation("Food") + " " + TranslationManager.Instance.GetTranslation("OnPlaying");
         } else if (mode == "scout")
         {
-            txtScout.text = TranslationManager.Instance.GetTranslation("ConfirmSure") + " " + node.GetComponent<Node>().scoutCost + " " + TranslationManager.Instance.GetTranslation("OnScouting");
+            txtHeader.text = TranslationManager.Instance.GetTranslation("ConfirmSure") + " " + node.GetComponent<Node>().scoutCost + " " + TranslationManager.Instance.GetTranslation("Food") + " " + TranslationManager.Instance.GetTranslation("OnScouting");
         }
     }
 }
