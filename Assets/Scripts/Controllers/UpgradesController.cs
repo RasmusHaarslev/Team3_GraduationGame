0135365d3196f9e0d1c0fac1b8b8ff621dbdf3ea
Peter Scheel <selech@live.dk>
2016-11-25 15:07:57 +0100

    no message

--- Assets/Scripts/Controllers/UpgradesController.cs
+++ Assets/Scripts/Controllers/UpgradesController.cs
@@ -5,21 +5,19 @@ using System;
 public class UpgradesController : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Awake() {
         var currentDate = DateTime.Today;
 
-        if (PlayerPrefs.HasKey(""))
+        if (PlayerPrefs.HasKey("GatherDate"))
         {
             var lastGather = DateTime.Parse(PlayerPrefs.GetString("GatherDate"));
 
             if(lastGather.Date < currentDate.Date){
                 var currentFood = PlayerPrefs.GetInt("Food");
-                PlayerPrefs.SetInt("Food", currentFood + (CampManager.Instance.Upgrades.GatherLevel * 3));
+                PlayerPrefs.SetInt("Food", currentFood + (CampManager.Instance.Upgrades.GatherLevel * 2));
             }
         }
 
-        PlayerPrefs.SetString("GatherDate", currentDate.ToString());
+        PlayerPrefs.SetString("GatherDate", (currentDate.ToString()));
     }
-
-    
 }
