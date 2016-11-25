e8147df57e33d9d6c3816ee5a576c405e367a062
John <irelandjj@gmail.com>
2016-11-25 14:42:50 +0100

    improved wepaon list

--- Assets/Scripts/Items/EquippableItemUIListController.cs
+++ Assets/Scripts/Items/EquippableItemUIListController.cs
@@ -8,105 +8,8 @@ public class EquippableItemUIListController : MonoBehaviour
     
     public GameObject ContentPanel;
     private GameObject ListItemPrefab;
-
-    //public EquippableitemValues[] itemsValues;
-    void OnEnable()
-    {
-        
-    }
-
-    void Start()
-    {
-        //ListItemPrefab = Resources.Load(StringResources.uiPrefabsPath + "EquippableItemUIScrollElement") as GameObject;
-        // 1. Get the data to be displayed
-        //itemsValues = new[] {
-        //      new EquippableitemValues
-        // {
-        //     id = 1,
-        //     name = "Toothpick",
-        //     Type = EquippableitemValues.type.polearm,
-        //     Slot = EquippableitemValues.slot.rightHand,
-        //     health = 20,
-        //     damage = 10,
-        //     damageSpeed = 9,
-        //     range = 5,
-        //     characterId = 1,
-        //     prefabName = "Stick"
-        // },
-        //     new EquippableitemValues
-        // {
-        //     id = 2,
-        //     name = "Plastic Shield",
-        //     Type = EquippableitemValues.type.shield,
-        //     Slot = EquippableitemValues.slot.rightHand,
-        //     health = 20,
-        //     damage = 10,
-        //     damageSpeed = 9,
-        //     range = 5,
-        //     characterId = 2,
-        //     prefabName = "Shield"
-        // },
-        //     new EquippableitemValues
-        // {
-        //     id = 3,
-        //     name = "Laser Rifle 2000",
-        //     Type = EquippableitemValues.type.rifle,
-        //     Slot = EquippableitemValues.slot.rightHand,
-        //     health = 20,
-        //     damage = 10,
-        //     damageSpeed = 9,
-        //     range = 20,
-        //     characterId = 3,
-        //     prefabName = "Rifle"
-        // },
-        //    new EquippableitemValues
-        //{
-        //    id = 4,
-        //    name = "Rifle of the Git Master Rasmus",
-        //    Type = EquippableitemValues.type.rifle,
-        //    Slot = EquippableitemValues.slot.rightHand,
-        //    health = 25,
-        //    damage = 15,
-        //    damageSpeed = 15,
-        //    range = 15,
-        //    prefabName = "Rifle"
-        //},
-        //     new EquippableitemValues
-        //     {
-        //         id = 5,
-        //         name = "Mighty power Stick",
-        //         Type = EquippableitemValues.type.polearm,
-        //         Slot = EquippableitemValues.slot.rightHand,
-        //         health = 20,
-        //         damage = 20,
-        //         damageSpeed = 9,
-        //         range = 5,
-        //         prefabName = "Stick"
-        //     },
-        //new EquippableitemValues
-        //{
-        //    id = 6,
-        //    name = "Romanian Steel Bar",
-        //    Type = EquippableitemValues.type.polearm,
-        //    Slot = EquippableitemValues.slot.rightHand,
-        //    health = 20,
-        //    damage = 25,
-        //    damageSpeed = 9,
-        //    range = 2,
-        //    prefabName = "Stick"
-        //}
-        //};
-
-        // 2. Iterate through the data, 
-        //	  instantiate prefab, 
-        //	  set the data, 
-        //	  add it to panel
-
-
-
-
-    }
-
+    //public GameObject cameraWeapons;
+    
    public void GenerateItemsList(IEnumerable<EquippableitemValues> itemsValues)
     {
         ListItemPrefab = Resources.Load(StringResources.uiPrefabsPath + "EquippableItemUIScrollElement") as GameObject;
@@ -127,6 +30,22 @@ public class EquippableItemUIListController : MonoBehaviour
             controller.health.text = "Health: " + values.health;
             controller.range.text = "Range: " + values.range;
             controller.itemValues = values;
+            
+            Debug.Log(controller.weaponCams.transform.GetChild(2).gameObject.GetComponent<Camera>().targetTexture);
+            Debug.Log(controller.Icon.texture);
+            if (values.Type.ToString() == "rifle")
+            {
+                controller.Icon.texture = controller.weaponCams.transform.GetChild(0).gameObject.GetComponent<Camera>().targetTexture;
+            }
+            if (values.Type.ToString() == "shield")
+            {
+                controller.Icon.texture = controller.weaponCams.transform.GetChild(1).gameObject.GetComponent<Camera>().targetTexture;
+            }
+            if (values.Type.ToString() == "polearm")
+            {
+                controller.Icon.texture = controller.weaponCams.transform.GetChild(2).gameObject.GetComponent<Camera>().targetTexture;
+            }
+
             newItem.transform.SetParent(ContentPanel.transform);
             newItem.transform.localScale = Vector3.one;
             newItem.transform.localPosition = Vector3.zero;
