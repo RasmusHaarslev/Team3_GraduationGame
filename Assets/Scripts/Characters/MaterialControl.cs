bfd9663784c11f6e05dd33a4e5581fe6138faf30
Veso <veselin.yovchev@hotmail.com>
2016-11-25 16:02:59 +0100

    made character materials

---
+++ Assets/Scripts/Characters/MaterialControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialControl : MonoBehaviour {
+	public Material[] possibleMaterials;
+	public SkinnedMeshRenderer mesh;
+	void OnEnable () 
+	{
+		mesh = GetComponentInChildren<SkinnedMeshRenderer> ();
+		mesh.sharedMaterial = possibleMaterials[Random.Range(0,possibleMaterials.Length)];
+	}
+
+}
