f453a9ac08bd5c574e78fd562ff6033392c4a4f3
Veso <veselin.yovchev@hotmail.com>
2016-11-29 09:28:48 +0100

    update bridges with sound

---
+++ Assets/Scripts/Audio/BridgeAudioControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeAudioControl : MonoBehaviour {
+
+	bool isSoundChanged = false;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.CompareTag("Player"))
+		{
+			if(!isSoundChanged)
+			{
+				isSoundChanged = true;
+				Manager_Audio.ChangeState (Manager_Audio.bridgeState,Manager_Audio.bridgeOn);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.CompareTag("Player"))
+		{
+			if(isSoundChanged)
+			{
+				isSoundChanged = false;
+				Manager_Audio.ChangeState (Manager_Audio.bridgeState,Manager_Audio.bridgeOff);
+			}
+		}
+	}
+}
