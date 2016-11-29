e6d40d828f9c71b008f0e6392beb7b32d7b4f590
Veso <veselin.yovchev@hotmail.com>
2016-11-29 12:49:35 +0100

    menu music control

---
+++ Assets/Scripts/Audio/MenuMusicControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusicControl : MonoBehaviour {
+
+	// Use this for initialization
+	void OnEnable () 
+	{
+		Manager_Audio.PlaySound(Manager_Audio.play_menuMusic, gameObject);
+		Manager_Audio.PlaySound(Manager_Audio.play_menuAmbience, gameObject);
+	}
+	
+	// Update is called once per frame
+	void OnDisable () 
+	{
+		Manager_Audio.PlaySound(Manager_Audio.stop_menuMusic, gameObject);
+		Manager_Audio.PlaySound(Manager_Audio.stop_menuAmbience, gameObject);
+	}
+}
