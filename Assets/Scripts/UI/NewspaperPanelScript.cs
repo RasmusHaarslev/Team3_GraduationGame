8a3d377e78da4d953e9250fe23d43f77a79eebdd
Peter Scheel <selech@live.dk>
2016-11-30 15:30:06 +0100

    no message

---
+++ Assets/Scripts/UI/NewspaperPanelScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NewspaperPanelScript : MonoBehaviour {
+
+    public Image NewspaperImage;
+
+	public void SetNewspaperImage(Sprite newspaper)
+    {
+        NewspaperImage.sprite = newspaper;
+    }
+}
