31cfe273c1c8209c499847728805c14ce94d5d4f
Andreea Muscalagiu <m_andreea93@yahoo.com>
2016-12-02 11:49:53 +0100

    video cutscene

---
+++ Assets/Scripts/Controllers/CutsceneController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CutsceneController : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartCutscene () {
+        ((MovieTexture)GetComponent<RawImage>().mainTexture).Play();
+    }
+}
