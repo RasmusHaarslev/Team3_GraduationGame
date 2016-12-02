581004090cefc21a0d69074a94cf721a2482693d
Andreea Muscalagiu <m_andreea93@yahoo.com>
2016-12-02 13:47:07 +0100

    no message

--- Assets/Scripts/Controllers/CutsceneController.cs
+++ Assets/Scripts/Controllers/CutsceneController.cs
@@ -4,8 +4,14 @@ using UnityEngine.UI;
 
 public class CutsceneController : MonoBehaviour {
 
+    void Start()
+    {
+        Invoke("StartCutscene", 1.0f);
+    }
+
 	// Use this for initialization
 	public void StartCutscene () {
-        ((MovieTexture)GetComponent<RawImage>().mainTexture).Play();
+        Debug.Log("hej");
+        Handheld.PlayFullScreenMovie("NewTest.mp4", Color.black, FullScreenMovieControlMode.Full);
     }
 }
