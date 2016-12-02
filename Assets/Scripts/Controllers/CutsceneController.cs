6f4549018bdb55992770f6d4c20a61dd1aa4fbe3
Christian Hansen <chr.hansen@live.dk>
2016-12-02 16:36:32 +0100

    changed few things

--- Assets/Scripts/Controllers/CutsceneController.cs
+++ Assets/Scripts/Controllers/CutsceneController.cs
@@ -2,6 +2,9 @@
 
 public class CutsceneController : MonoBehaviour
 {
+
+	public GameObject blackScreen;
+
     void Start()
     {
         Invoke("StartCutscene", 3.0f);
@@ -10,6 +13,7 @@ public class CutsceneController : MonoBehaviour
 	// Use this for initialization
 	private void StartCutscene ()
 	{
+		blackScreen.SetActive(true);
         Handheld.PlayFullScreenMovie("soundtest.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
         GameController.Instance.LoadScene("TutorialLevel01");
     }
