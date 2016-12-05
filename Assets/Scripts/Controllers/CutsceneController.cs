833c5cc67fa667e460e539d479ed1f42d0420bdf
Andreea Muscalagiu <m_andreea93@yahoo.com>
2016-12-05 14:46:04 +0100

    added cutscenes

--- Assets/Scripts/Controllers/CutsceneController.cs
+++ Assets/Scripts/Controllers/CutsceneController.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
 public class CutsceneController : MonoBehaviour
 {
+    public string cutscene;
+    public string scene;
+    public Boolean randomLevel;
+
     void Start()
     {
         Invoke("StartCutscene", 3.0f);
@@ -12,7 +17,11 @@ public class CutsceneController : MonoBehaviour
 	// Use this for initialization
 	private void StartCutscene ()
 	{
-        Handheld.PlayFullScreenMovie("Cutscene1.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
-        GameController.Instance.LoadScene("TutorialLevel01");
+        Handheld.PlayFullScreenMovie(cutscene, Color.black, FullScreenMovieControlMode.CancelOnInput);
+
+	    if (randomLevel)
+            GameController.Instance.LoadLevel();
+        else
+            GameController.Instance.LoadScene(scene);
     }
 }
