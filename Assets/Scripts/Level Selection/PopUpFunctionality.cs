d9c5f1243ee2f438bbf34afff064d1555d169e06
Nicolai <nico_goal@hotmail.com>
2016-12-06 17:48:12 +0100

    no message

--- Assets/Scripts/Level Selection/PopUpFunctionality.cs
+++ Assets/Scripts/Level Selection/PopUpFunctionality.cs
@@ -199,7 +199,8 @@ public class PopUpFunctionality : MonoBehaviour {
     public void AcceptPlay(GameObject node)
     {
         Manager_Audio.PlaySound(Manager_Audio.play_intoLevel, gameObject);
-        EventManager.Instance.TriggerEvent(new ChangeResources(-node.GetComponent<Node>().TravelCost));        
+        EventManager.Instance.TriggerEvent(new ChangeResources(-node.GetComponent<Node>().TravelCost));
+        EventManager.Instance.TriggerEvent(new LevelStarted());
 
         PlayerPrefs.SetInt(StringResources.NodeIdPrefsName, node.GetComponent<Node>().NodeId);
         PlayerPrefs.SetInt(StringResources.LevelDifficultyPrefsName, node.GetComponent<Node>().Level);
