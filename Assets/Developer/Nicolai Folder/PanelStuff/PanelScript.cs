532741c5c59e3a5cd9f3b4618c9cfe67782ac403
John <irelandjj@gmail.com>
2016-12-08 14:39:23 +0100

    no message

--- Assets/Developer/Nicolai Folder/PanelStuff/PanelScript.cs
+++ Assets/Developer/Nicolai Folder/PanelStuff/PanelScript.cs
@@ -602,7 +602,7 @@ public class PanelScript : MonoBehaviour
                 }
                 if (stat.name == "Soldier Name")
                 {
-                    stat.GetComponent<Text>().text = TranslationManager.Instance.GetTranslation(newCharacterSoldierList[i].name); 
+                    stat.GetComponent<Text>().text = newCharacterSoldierList[i].name; 
                 }
                 if (stat.name == "Health")
                 {
